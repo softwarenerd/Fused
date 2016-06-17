f2c68edab0a161b233708b925d9786711d942b23
Brian Lambert <brianlambert@gmail.com>
2016-06-17 16:01:45 -0400

    First commit.

---
+++ Fused/Fused.h
@@ -0,0 +1,20 @@
+//
+//  Fused.h
+//  Fused
+//
+//  Created by Brian Lambert on 6/17/16.
+//  Copyright © 2016 Brian Lambert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fused.
+FOUNDATION_EXPORT double FusedVersionNumber;
+
+//! Project version string for Fused.
+FOUNDATION_EXPORT const unsigned char FusedVersionString[];
+
+// Public headers.
+#import <Fused/MadgwickSensorFusion.h>
+#import <Fused/CoreMotionMadgwickTestDriver.h>
+
