5784938c4e9878a7e4d55c7b393daec9288c4250
Brian Lambert <brianlambert@gmail.com>
2016-06-20 20:20:50 -0400

    Set up to add Mahony now.

--- Fused/Fused.h
+++ Fused/Fused.h
@@ -15,6 +15,7 @@ FOUNDATION_EXPORT double FusedVersionNumber;
 FOUNDATION_EXPORT const unsigned char FusedVersionString[];
 
 // Public headers.
+#impirt <Fused/SensorFusionProcessor.h>
 #import <Fused/MadgwickSensorFusion.h>
 #import <Fused/MahonySensorFusion.h>
 #import <Fused/CoreMotionTestDriver.h>
