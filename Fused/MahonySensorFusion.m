95323a16eeb69822129f5a796818f9220a1b4bf1
Brian Lambert <brianlambert@gmail.com>
2016-06-26 18:02:06 -0400

    A few small updates.

--- Fused/MahonySensorFusion.m
+++ Fused/MahonySensorFusion.m
@@ -18,12 +18,13 @@
 // Inverse square root.
 static inline float invSqrt(float x)
 {
-    float xhalf = 0.5f * x;
-    int i = *(int*)&x;              // get bits for floating value
-    i = 0x5f375a86 - (i >> 1);      // gives initial guess y0
-    x = *(float*)&i;                // convert bits back to float
-    x = x * (1.5f - xhalf * x * x); // Newton step, repeating increases accuracy
-    return x;
+    return 1.0f / sqrtf(x);
+//    float xhalf = 0.5f * x;
+//    int i = *(int*)&x;              // get bits for floating value
+//    i = 0x5f375a86 - (i >> 1);      // gives initial guess y0
+//    x = *(float*)&i;                // convert bits back to float
+//    x = x * (1.5f - xhalf * x * x); // Newton step, repeating increases accuracy
+//    return x;
 }
 
 // MahonySensorFusion implementation.
